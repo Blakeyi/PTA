1aa6fe36b8442ec4016aea55823dd7c431f55308
Blakeyi <1152420971@qq.com>
2019-06-26 22:50:21 +0800

    ReAC

--- L2_014_TrainRank.cpp
+++ L2_014_TrainRank.cpp
@@ -1,50 +1,68 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
 
+// int main()
+// {
+//   int rear[1000];
+//   int max, x, n, count = 0;
+//   cin >> n;
+//   cin >> rear[0];
+//   count++;
+//   max = rear[0];
+//   for (int i = 0; i < n - 1; i++)
+//   {
+//     cin >> x;
+
+//     int count2 = 0;
+//     while (count2 < count)
+//     {
+//       if (rear[count2] > max)
+//         max = rear[count2];
+//       if (x > rear[count2])
+//       {
+//         count2++;
+//         if (count2 == count)
+//         {
+//           count++;
+//           rear[count - 1] = x;
+//           break;
+//         }
+//       }
+//       else
+//       {
+
+//         rear[count2] = x;
+//         break;
+//       }
+//     }
+//     sort(rear, rear + count2);
+//   }
+
+//   cout << count;
+
+//   return 0;
+// }
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+using namespace std;
 int main()
 {
-  int rear[1000];
-  int max, x, n, count = 0;
+  set<int> s;
+  int n, num;
   cin >> n;
-  cin >> rear[0];
-  count++;
-  max = rear[0];
-  for (int i = 0; i < n - 1; i++)
+  for (int i = 0; i < n; i++)
   {
-    cin >> x;
-    if (x > max)
-    {
-      count++;
-      rear[count - 1] = x;
+    cin >> num;
+    if (s.upper_bound(num) != s.end()) //set会按照从小到大排序
+    {                                  //num只要大于的不是最后一个元素，就可以插到老队伍里
+      //upper_bound求出第一个大于num的下标
+      s.erase(s.upper_bound(num));
     }
-    else
-    {
-      int count2 = 0;
-      while (count2 < count)
-      {
-        if (rear[count2] > max)
-          max = rear[count2];
-        if (x > rear[count2])
-        {
-          count2++;
-          if (count2 == count)
-          {
-            count++;
-            rear[count - 1] = x;
-            break;
-          }
-        }
-        else
-        {
-
-          rear[count2] = x;
-          break;
-        }
-      }
-    }
+    s.insert(num);
   }
-  cout << count;
-
-  return 0;
+  cout << s.size();
 }
